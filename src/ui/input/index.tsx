9ecba89a9ea8e870ca56edb8dbec761eb4b8ad6b
jongmyeong <polcaneli@gmail.com>
2021-12-09 17:08:37 +0900

    refactor(@ui): input 개선 * props: error -> invalid로 변경 * ref가 투영되게 개선 * onFocus, onBlur 함수도 연결되게 개선

--- src/ui/input/index.tsx
+++ src/ui/input/index.tsx
@@ -1,19 +1,21 @@
-import React, { InputHTMLAttributes, useState } from 'react';
+import React, { HTMLAttributes, InputHTMLAttributes, useState } from 'react';
 import classNames from 'classnames';
 import { prefix } from '../core';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  error?: boolean;
+  invalid?: boolean;
 }
 
 const inputClsName = `${prefix}-input`;
-const Input: React.FC<InputProps> = (props) => {
+const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   const {
     children,
     className,
     disabled,
-    error,
+    invalid,
     type = 'text',
+    onFocus,
+    onBlur,
     ...otherProps
   } = props;
   const [focus, isFocus] = useState(false);
@@ -23,7 +25,7 @@ const Input: React.FC<InputProps> = (props) => {
         inputClsName,
         {
           [`${inputClsName}-focus`]: focus,
-          [`${inputClsName}-error`]: error,
+          [`${inputClsName}-invalid`]: invalid,
           [`${inputClsName}-disabled`]: disabled,
         },
         className,
@@ -34,12 +36,30 @@ const Input: React.FC<InputProps> = (props) => {
           disabled={disabled}
           type={type}
           {...otherProps}
-          onFocus={() => isFocus(true)}
-          onBlur={() => isFocus(false)}
+          onFocus={(e) => {
+            isFocus(true);
+            onFocus && onFocus(e);
+          }}
+          onBlur={(e) => {
+            isFocus(false);
+            onBlur && onBlur(e);
+          }}
+          ref={ref}
         >
           {children}
         </input>
       </div>
+    </div>
+  );
+});
+
+export const InputErrorMessage: React.FC<HTMLAttributes<HTMLDivElement>> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div className={classNames(`${inputClsName}-error-message`, className)}>
+      {children}
     </div>
   );
 };
