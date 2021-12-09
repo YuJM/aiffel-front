9dd71e279620cee9d6787af3fafe489e22e861e2
jongmyeong <polcaneli@gmail.com>
2021-12-09 20:03:55 +0900

    feat: redux store 적용

--- src/pages/loginPage/index.tsx
+++ src/pages/loginPage/index.tsx
@@ -6,14 +6,17 @@ import { loginURL } from '@model/http';
 import { TFormData } from '@model/auth';
 import { useDispatch } from 'react-redux';
 import { saveSession } from '@store/authSlice';
+import { useNavigate } from 'react-router';
 
 const LoginPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const onSubmit = useCallback((data: TFormData) => {
     axios
       .get(loginURL, { params: { data } })
       .then(({ data }) => {
         dispatch(saveSession(data[0]));
+        navigate('/forum');
       })
       .catch((e) => {
         console.error(e);
