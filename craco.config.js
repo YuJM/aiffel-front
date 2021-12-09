6258321cca7221df83552f56f50de4dc8d528c65
jongmyeong <polcaneli@gmail.com>
2021-12-09 11:52:05 +0900

    init: 필요한 패키지 추가, tsconfig 상대경로 작업

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@model': path.resolve(__dirname, 'src/util'),
+      '@util': path.resolve(__dirname, 'src/util'),
+      '@ui': path.resolve(__dirname, 'src/ui'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+    },
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^@(.*)$': '<rootDir>/src$1',
+      },
+    },
+  },
+};
