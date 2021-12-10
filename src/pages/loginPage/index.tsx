f196c282f803381db9cd0ec2eca82732bd3525d1
jongmyeong <polcaneli@gmail.com>
2021-12-10 23:19:37 +0900

    refactor(@pages): login page * toast 추가 * 스타일 변경

--- src/pages/loginPage/index.tsx
+++ src/pages/loginPage/index.tsx
@@ -3,27 +3,32 @@ import './login_page.scss';
 import axios from 'axios';
 import { useCallback } from 'react';
 import { loginURL } from '@model/http';
-import { TFormData } from '@model/auth';
+import { TLoginForm } from '@model/auth';
 import { useDispatch } from 'react-redux';
 import { saveSession } from '@store/authSlice';
 import { useNavigate } from 'react-router';
+import { toast } from 'react-hot-toast';
 
 const LoginPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const onSubmit = useCallback((data: TFormData) => {
-    axios
-      .get(loginURL, { params: { data } })
-      .then(({ data }) => {
-        dispatch(saveSession(data[0]));
-        navigate('/forum');
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  }, []);
+  const onSubmit = useCallback(
+    (data: TLoginForm) => {
+      axios
+        .get(loginURL, { params: { data } })
+        .then(({ data }) => {
+          dispatch(saveSession(data[0]));
+          navigate('/forum');
+        })
+        .catch((e) => {
+          console.error(e);
+          toast.error('아이디와 비밀번호를 확인해주세요');
+        });
+    },
+    [dispatch, navigate],
+  );
   return (
-    <main>
+    <main className={'login-page'}>
       <LoginForm onSubmit={onSubmit} />
     </main>
   );
